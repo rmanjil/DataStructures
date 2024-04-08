d4e1a1b88fa410fd06d95d890615efcecbfddd02
Manjil <manjil.rajbhandari@fonenxt.com>
2024-04-08 14:42:16 +0545

    linkedList: feat: create link list

--- DataStructures.swiftpm/ContentView.swift
+++ DataStructures.swiftpm/ContentView.swift
@@ -6,7 +6,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world!").onAppear {
+                createLinkedList()
+            }
         }
     }
 }
--- DataStructures.swiftpm/LinkedList.swift
+++ DataStructures.swiftpm/LinkedList.swift
@@ -26,4 +26,16 @@ extension Node: CustomStringConvertible {
     }
 }
 
-
+func createLinkedList() {
+    example(of: "Create and Linking nodes") {
+        let node1 = Node(value: 1)
+        let node2 = Node(value: 2)
+        let node3 = Node(value: 3)
+        
+        node1.next = node2
+        node2.next = node3
+        
+        print(node1)
+    }
+    
+}
