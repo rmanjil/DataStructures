f551265ecceaf7d0c045454cff0a08ae71882bc2
Manjil <manjil.rajbhandari@fonenxt.com>
2024-05-20 11:47:45 +0545

    stack: feat: Inherit CustomStringConvertible by Stack

--- DataStructures.swiftpm/Stack.swift
+++ DataStructures.swiftpm/Stack.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct Stack<Element> {
+    private var storage: [Element] = []
+    init() {}
+}
+
+
+extension Stack: CustomStringConvertible {
+    var description: String {
+        let topDivider = "----top----\n"
+        let bottomDivider = "\n-------------"
+        
+        let stackElements = storage.map { "\($0)" }.reversed().joined(separator: "\n")
+        return topDivider + stackElements + bottomDivider
+    }
+}
